de4d32efd9f64673cd2af907ee5bd1fcebbcd2ec
vanelo <vanessa@muv-app.co>
2021-07-01 22:49:33 -0400

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Remove line below to enable local ValidationPipe settings
   app.useGlobalPipes(new ValidationPipe());
+  // Enable cors
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
